6bd784262129c79cbb4f3ffa54679e1559d1a3c7
DJA <devanjay@gmail.com>
2023-02-13 12:27:56 -0500

    Check added to avoid duplicate links.
    Recursive version for more link depths commented out, because it was slow to read the html code of all the links that were outputted (even for one layer of depth). However, the code is still there (ctrl+f "RECURSIVE CODE" if you want to add it).

--- src/HtmlRead.java
+++ src/HtmlRead.java
@@ -5,6 +5,8 @@ import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashSet;
 //concat
 //contains***
 //compareto
@@ -14,9 +16,13 @@ import java.net.URL;
 //
 
 
-public class HtmlRead implements ActionListener {
-
+public class HtmlRead  {
+    public static int n = 1;
+    public static ArrayList<HashSet<String>> list = new ArrayList<HashSet<String>>();
     public static void main(String[] args) {
+        for(int i = 0; i<n; i++) {
+            list.add(new HashSet<String>());
+        }
         JFrame mf = new JFrame("Link Searcher");
         mf.setSize(1000, 800);
         mf.setLayout(new GridLayout(2, 1));
@@ -51,15 +57,16 @@ public class HtmlRead implements ActionListener {
         start.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                HtmlRead html = new HtmlRead(link.getText(), search.getText(), bottom);
+                bottom.setText("");
+                HtmlRead html = new HtmlRead(link.getText(), search.getText(), bottom, 0);
 
             }
         });
 
     }
 
-    public HtmlRead(String link, String search, JTextArea bottom) {
-        bottom.setText("");
+    public HtmlRead(String link, String search, JTextArea bottom, int index) {
+        bottom.setVisible(true);
         try {
             System.out.println();
             System.out.print("hello \n");
@@ -69,16 +76,28 @@ public class HtmlRead implements ActionListener {
             );
             String line;
             while ( (line = reader.readLine()) != null ) {
-                    parseLine(line, search, bottom);
+                    parseLine(line, search, bottom, index);
             }
             reader.close();
+            if(bottom.getText().length()==0) {
+                bottom.setText("No Links Found That Match Search Term");
+            }
+            HashSet<String> newC = list.get(index);
+//            RECURSIVE CODE (N is depth):
+//            if(index<n) {
+//                for(String s: newC) {
+//                    new HtmlRead(s, search, bottom, index+1);
+//                }
+//            }
         } catch(Exception ex) {
             System.out.println("error?");
-//            bottom.setText("Exception: Invalid Link");
+            if(index==0) {
+                bottom.setText("Exception: Invalid Link");
+            }
         }
 
     }
-    public void parseLine(String line, String search, JTextArea bottom) {
+    public void parseLine(String line, String search, JTextArea bottom, int index) {
         String[] starters = new String[]{"href='","href=\"", "src='", "src=\""};
         for(String s: starters) {
             if (line.contains(s)) {
@@ -95,20 +114,19 @@ public class HtmlRead implements ActionListener {
                     }
                 }
                 String link = line.substring(start + plus, end);
-                if(link.contains(search)) {
+                if(link.toLowerCase().contains(search.toLowerCase()) && !bottom.getText().contains(link)) {
                     bottom.append(link + "\n\n");
+                    list.get(index).add(link);
                 }
                 if(end==x.length) return;
                 line = line.substring(end+1);
-                parseLine(line, search, bottom);
+                parseLine(line, search, bottom, index);
                 break;
             }
 
         }
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
 
     }
+
+
 }
